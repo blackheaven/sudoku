6272f74a5ddad761f3c31deac4b719d0319cb87f
Troll <trolldu26@gmail.com>
2012-10-16 20:49:11 +0200

    Now able to read from a file and generate the grid from the file

--- parsing_files.pl
+++ parsing_files.pl
@@ -1,24 +1,4 @@
-integer(I) -->
-        digit(D0),
-        digits(D),
-        { number_codes(I, [D0|D])
-        }.
-
-digits([D|T]) -->
-        digit(D), !,
-        digits(T).
-digits([]) -->
-        [].
-
-digit(D) -->
-        [D],
-        { code_type(D, digit)
-        }.
-        
-ints([], _, _).
-ints(Sentence, Result, R) :- phrase(integer(X), Sentence, Rest), append([X], Result, R), ints(Rest, R, R2), R = R2.
-
-t([A|_]) :- A is 0'a.
+zone(L, C, Z) :- Z is (1 + floor((C - 1)/3) + 3 * floor((L - 1)/3)).
 
 %% i(): true if the 2nd parameter contains the list of all 
 %% integers contained in the string (passed as ASCII list) 
@@ -28,7 +8,7 @@ t([A|_]) :- A is 0'a.
 digit_code(J) :- J >= 0'0, J =< 0'9.
 i([], []).
 i([], _) :- fail.
-i([A|S], [A|R]) :- digit_code(A),!, i(S, R).
+i([A|S], [B|R]) :- (digit_code(A), B=A; (A=0'_, B is 48)),!, i(S, R). % if the char is not a number but is a "_", we evaluate it to 0 (48)
 i([_|S], R) :- i(S, R).
 
 %% transf(): true if the 2nd parameter is the list of integers of the 1st parameter subtracted 48
@@ -42,5 +22,54 @@ transf([A|S], [B|R]) :- B is A-48, transf(S, R).
 % another example usage: i("1 2 3", [1, 2, 3]).
 super_i(Sentence, R) :- i(Sentence, A), transf(A, R).
 
+grid([], _).
+% grid(L, R).
+
+read_lines(T) :- 
+  open('./grille.txt', read, F),!, read_lines(T, F), close(F).
+
+read_lines(T, F) :- 
+  read_line_to_codes(F, Temp), 
+  (Temp=end_of_file, T = [] ; 
+        read_lines(T2, F),
+        super_i(Temp, H), 
+        append(H, T2, T)
+  )
+  
+  % to "return" an empty value in the case we reach the end of file, instead of having it un-instantiated
+  .
+
+transf2([], _).
+transf2(G, R) :- 
+  transf2(G, 1, R)
+.
+transf2([], _, []).
+
+transf2([0|G], N, R) :- 
+  N2 is N+1,
+  transf2(G, N2, R)
+.
+
+transf2([A|G], N, [v(p(L,C,Z), A)|R]) :- 
+  C is mod(N, 9), % col does not go after 9
+  L is floor(N/9)+1, % line is floor(col/9)+1
+  zone(L, C, Z),
+  N2 is N+1,
+  transf2(G, N2, R)
+.
+
+%% Somewhere remplis(1, 1, Grille), transf2(Cases, Grille, Remplies, Restantes) %%, complete(Remplies, Couleurs, RestantesBrute)
+% transf2([], _, [], []).
+% transf2([0|CS], [G|GS], D, [G|TS]) :-
+%       transf2(CS, GS, D, TS).
+% transf2([C|CS], [G|GS], [v(G, C)|D], TS) :-
+%       transf2(CS, GS, D, TS).
+
+
+test(A) :-
+        %trace,
+        read_lines(L),
+        !,
+        transf2(L, A), !.
 % @TODO: find a way to get the line as an ASCII list (with read() ? read_lines_to_codes() ? ) and then use super_i()
 % @TODO: Then, use the same way remplis() does to generate a grid (zone(X, Y, X), v(p(X, Y, Z), Value))
