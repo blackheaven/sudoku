e0ffc855b04ec7cf4a9e2c6c857be3e3eee6635e
Troll <trolldu26@gmail.com>
2012-10-16 22:25:54 +0200

    Merging to main file (and renaming it btw). Some bugs in the grid generation (from file) Now reading and solving. But the solver seems to have some issues with some grids...

--- sudoku_prolog.pl
+++ sudoku_prolog.pl
@@ -33,18 +33,6 @@ v(p(9, 4, 8), 6),
 v(p(9, 6, 8), 1),
 v(p(9, 9, 9), 2)].
 
-test(S) :- 
-    grille(G),
-    sudoku_solve(G, [1, 2, 3, 4, 5, 6, 7, 8, 9], S)
-    .
-
-solve :-
-    test(R),
-    open('grille-res.txt', write, S),
-    predsort(sort_res, R, T),
-    prettyprint(S, T),
-    nl(S),
-    close(S), !.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% prettyprint prints to the output in an elegant way %%
@@ -176,3 +164,104 @@ sort_res(>, v(p(L1, C1, _), _), v(p(L2, C2, _), _)) :-
     P2 is L2 * 10 + C2,
     P1 >= P2.
 sort_res(<, _, _).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% BATCH PROCESSING %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% i(): true if the 2nd parameter contains the list of all 
+%% integers contained in the string (passed as ASCII list) 
+%% passed as 1st parameter
+% example usage: i("1 2 3", [49, 50, 51]).
+% another example usage: i("1 _ 3", [49, 51]).
+digit_code(J) :- J >= 0'0, J =< 0'9.
+i([], []).
+i([], _) :- fail.
+i([A|S], [B|R]) :- (digit_code(A), B=A; (A=0'_, B is 48)),!, i(S, R). % if the char is not a number but is a "_", we evaluate it to 0 (48)
+i([_|S], R) :- i(S, R).
+
+%% transf(): true if the 2nd parameter is the list of integers of the 1st parameter subtracted 48
+%% used to transform a list of digits ASCII codes into a list of those digits...
+% example usage: transf([0,0], [-48, -48]).
+% another example usage: transf([48, 48], L). L=[0,0]
+transf([], []).
+transf([A|S], [B|R]) :- B is A-48, transf(S, R).
+
+% example usage: super_i("1 _ 3", [1, 3]).
+% another example usage: i("1 2 3", [1, 2, 3]).
+super_i(Sentence, R) :- i(Sentence, A), transf(A, R).
+
+grid([], _).
+% grid(L, R).
+
+read_lines(T) :- 
+  open('./grille.txt', read, F),!, read_lines(T, F), close(F).
+
+read_lines(T, F) :- 
+  read_line_to_codes(F, Temp), 
+  (Temp=end_of_file, T = [] ; 
+        read_lines(T2, F),
+        super_i(Temp, H), 
+        append(H, T2, T)
+  )
+  
+  % to "return" an empty value in the case we reach the end of file, instead of having it un-instantiated
+  .
+
+transf2([], _).
+transf2(G, R) :- 
+  transf2(G, 1, R)
+.
+transf2([], _, []).
+
+transf2([0|G], N, R) :- 
+  N2 is N+1,
+  transf2(G, N2, R)
+.
+
+transf2([A|G], N, [v(p(L,C,Z), A)|R]) :- 
+  C_ is mod(N, 9), % col does not go after 9
+  ((C_ \= 0, C=C_) ; C = 9),
+  L is ceil(N/9), % line is ceil(N/9) (1-9 = 1, 2-18=2, etc. ...)
+  zone(L, C, Z),
+  N2 is N+1,
+  transf2(G, N2, R)
+.
+
+%% Somewhere remplis(1, 1, Grille), transf2(Cases, Grille, Remplies, Restantes) %%, complete(Remplies, Couleurs, RestantesBrute)
+% transf2([], _, [], []).
+% transf2([0|CS], [G|GS], D, [G|TS]) :-
+%       transf2(CS, GS, D, TS).
+% transf2([C|CS], [G|GS], [v(G, C)|D], TS) :-
+%       transf2(CS, GS, D, TS).
+
+test(0, _).
+test(N, [Nam|FR]) :-
+        %trace,
+        number_codes(N, Codes),
+        append("solutions/", Codes, Sol_),
+        append("grilles/", Codes, Name_),
+        append(Name_, ".txt", Na),
+        append(Sol_, ".txt", So),
+        string_to_list(Nam, Na),
+        string_to_list(Sol, So),
+        open(Nam, read, F),
+        read_lines(L, F),
+        close(F),
+        !,
+        transf2(L, G), !,
+        open(Sol, write, S),
+        % debug stuff: 
+        %trace,
+        %write(S, G),
+        sudoku_solve(G, [1, 2, 3, 4, 5, 6, 7, 8, 9], R),
+        predsort(sort_res, R, T),
+        prettyprint(S, T),
+        nl(S),
+        close(S), !,
+        N2 is N-1,
+        test(N2, FR)
+.
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
