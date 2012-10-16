733cbb63058d1f760bdd6f200bf5676933348b68
Gautier DI FOLCO <gautier.difolco@gmail.com>
2012-10-17 00:22:25 +0200

    Algo + grilles

--- sudoku_prolog.pl
+++ sudoku_prolog.pl
@@ -1,38 +1,6 @@
-grille(X) :- X = [
-v(p(1, 1, 1), 4),
-v(p(1, 4, 2), 5),
-v(p(1, 6, 2), 3),
-v(p(1, 7, 3), 7),
-v(p(1, 8, 3), 6),
-v(p(2, 1, 1), 5),
-v(p(2, 4, 2), 1),
-v(p(2, 6, 2), 7),
-v(p(3, 5, 2), 9),
-v(p(3, 8, 3), 3),
-v(p(4, 1, 4), 2),
-v(p(4, 2, 4), 5),
-v(p(4, 4, 5), 9),
-v(p(4, 7, 6), 1),
-v(p(5, 2, 4), 9),
-v(p(5, 3, 4), 6),
-v(p(5, 5, 5), 1),
-v(p(5, 7, 6), 3),
-v(p(5, 8, 6), 2),
-v(p(6, 3, 4), 3),
-v(p(6, 6, 5), 6),
-v(p(6, 8, 6), 9),
-v(p(6, 9, 6), 8),
-v(p(7, 2, 7), 2),
-v(p(7, 5, 8), 4),
-v(p(8, 4, 8), 8),
-v(p(8, 6, 8), 5),
-v(p(8, 9, 9), 3),
-v(p(9, 2, 7), 8),
-v(p(9, 3, 7), 5),
-v(p(9, 4, 8), 6),
-v(p(9, 6, 8), 1),
-v(p(9, 9, 9), 2)].
-
+test(G, S) :-
+    sudoku_solve(G, [1, 2, 3, 4, 5, 6, 7, 8, 9], S)
+    .
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% prettyprint prints to the output in an elegant way %%
@@ -47,21 +15,25 @@ prettyprint(F, [v(_, V)|N]):- write(F, V), write(F, ' '), prettyprint(F, N).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% sudoku_solve solves the sudoku %%
 
-sudoku_solve(Remplies, Couleurs, Solutions) :-
-	% init
-    complete(Remplies, Couleurs, RestantesBrute),
-    predsort(sort_cases, RestantesBrute, Restantes),
+sudoku_solve(Entrees, Couleurs, Solutions) :-
+    % init
+    remplis(1, 1, CasesBrute),
+    predsort(sort_cases, CasesBrute, Cases),
+    transf2(Entrees, Cases, Remplies, Manquantes),
+    pondere(Manquantes, Remplies, Couleurs, RestantesBrutes),
+    !,
+    predsort(sort_cases, RestantesBrutes, Restantes),
     % Solving
-    resout(Restantes, Remplies, Solutions).
+    !,
+    resout(Restantes, Remplies, Solutions),
+    !.
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Completes
-complete(Remplies, Couleurs, Restantes) :-
-    remplis(1, 1, Cases),
-    filtre_existantes(Cases, Remplies, Manquantes), !,
-    pondere(Manquantes, Remplies, Couleurs, Restantes).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% transf2(Cases, Grille, Remplies, Restantes)
+transf2([], _, [], []).
+transf2([0|CS], [G|GS], D, [G|TS]) :-
+      transf2(CS, GS, D, TS).
+transf2([C|CS], [G|GS], [v(G, C)|D], TS) :-
+      transf2(CS, GS, D, TS).
 
 remplis(9, 10, []).
 remplis(L, 10, R) :-
@@ -79,16 +51,6 @@ zone(L, C, Z) :- Z is (1 + floor((C - 1)/3) + 3 * floor((L - 1)/3)).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-filtre_existantes([], _, []).
-filtre_existantes([C|S], Remplies, [C|R]) :-
-    not(memberchk(v(C, _), Remplies)),
-    filtre_existantes(S, Remplies, R).
-filtre_existantes([C|S], Remplies, R) :-
-    memberchk(v(C, _), Remplies),
-    filtre_existantes(S, Remplies, R).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 pondere([], _, _, []).
 pondere([M|MS], Remplies, Couleurs, [v(M, P)|RS]) :-
     voisins(M, Remplies, Vb),
@@ -102,10 +64,10 @@ pondere([M|MS], Remplies, Couleurs, [v(M, P)|RS]) :-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 voisins(_, [], []).
-voisins(M, [v(R, V)|RS], [v(R, V)|VS]) :- 
+voisins(M, [v(R, V)|RS], [v(R, V)|VS]) :-
     voisin(M, R),
     voisins(M, RS, VS).
-voisins(M, [R|RS], VS) :- 
+voisins(M, [R|RS], VS) :-
     not(voisin(M, R)),
     voisins(M, RS, VS).
 
@@ -130,12 +92,11 @@ resout([M|MS], Remplies, Solutions) :-
     NT = [M|MS],
     predsort(sort_cases, NT, N),
     [v(P, F)|NS] = N,
-    % write(v(P, F)), nl,
     (not(F = [_]) ->
         (member(C, F),
          repondere(C, P, NS, NMS),
          resout(NMS, [v(P, C)|Remplies], Solutions)) ;
-        resout(N, Remplies, Solutions)).
+         resout(N, Remplies, Solutions)).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -151,10 +112,10 @@ repondere(V, Pa, [v(M, P)|MS], [v(M, P)|RS]) :-
     repondere(V, Pa, MS, RS).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Comparison routines, used by the predsort() predicate %% 
+%% Comparison routines, used by the predsort() predicate %%
 
 sort_cases(>, v(_, X), v(_, Y)) :-
-    length(X, Xn), 
+    length(X, Xn),
     length(Y, Yn),
     Xn >= Yn.
 sort_cases(<, _, _).
@@ -165,8 +126,27 @@ sort_res(>, v(p(L1, C1, _), _), v(p(L2, C2, _), _)) :-
     P1 >= P2.
 sort_res(<, _, _).
 
+% integer(I) -->
+%         digit(D0),
+%         digits(D),
+%         { number_codes(I, [D0|D])
+%         }.
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%% BATCH PROCESSING %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% digits([D|T]) -->
+%         digit(D), !,
+%         digits(T).
+% digits([]) -->
+%         [].
+
+% digit(D) -->
+%         [D],
+%         { code_type(D, digit)
+%         }.
+
+% ints([], _, _).
+% ints(Sentence, Result, R) :- phrase(integer(X), Sentence, Rest), append([X], Result, R), ints(Rest, R, R2), R = R2.
+
+% t([A|_]) :- A is 0'a.
 
 %% i(): true if the 2nd parameter contains the list of all 
 %% integers contained in the string (passed as ASCII list) 
@@ -193,75 +173,43 @@ super_i(Sentence, R) :- i(Sentence, A), transf(A, R).
 grid([], _).
 % grid(L, R).
 
-read_lines(T) :- 
-  open('./grille.txt', read, F),!, read_lines(T, F), close(F).
 
-read_lines(T, F) :- 
+read_lines(F, T) :- 
   read_line_to_codes(F, Temp), 
   (Temp=end_of_file, T = [] ; 
-        read_lines(T2, F),
+        read_lines(F, T2),
         super_i(Temp, H), 
         append(H, T2, T)
   )
-  
   % to "return" an empty value in the case we reach the end of file, instead of having it un-instantiated
   .
 
-transf2([], _).
-transf2(G, R) :- 
-  transf2(G, 1, R)
-.
-transf2([], _, []).
 
-transf2([0|G], N, R) :- 
-  N2 is N+1,
-  transf2(G, N2, R)
-.
+% @TODO: find a way to get the line as an ASCII list (with read() ? read_lines_to_codes() ? ) and then use super_i()
+% @TODO: Then, use the same way remplis() does to generate a grid (zone(X, Y, X), v(p(X, Y, Z), Value))
 
-transf2([A|G], N, [v(p(L,C,Z), A)|R]) :- 
-  C_ is mod(N, 9), % col does not go after 9
-  ((C_ \= 0, C=C_) ; C = 9),
-  L is ceil(N/9), % line is ceil(N/9) (1-9 = 1, 2-18=2, etc. ...)
-  zone(L, C, Z),
-  N2 is N+1,
-  transf2(G, N2, R)
-.
+process(I, O) :-
+    open(I, read, F),
+    read_lines(F, L),
+    close(F),
+    test(L, R),
+    open(O, write, S),
+    predsort(sort_res, R, T),
+    prettyprint(S, T),
+    nl(S),
+    close(S), !.
 
-%% Somewhere remplis(1, 1, Grille), transf2(Cases, Grille, Remplies, Restantes) %%, complete(Remplies, Couleurs, RestantesBrute)
-% transf2([], _, [], []).
-% transf2([0|CS], [G|GS], D, [G|TS]) :-
-%       transf2(CS, GS, D, TS).
-% transf2([C|CS], [G|GS], [v(G, C)|D], TS) :-
-%       transf2(CS, GS, D, TS).
-
-test(0, _).
-test(N, [Nam|FR]) :-
+solve(0).
+solve(N) :-
         %trace,
         number_codes(N, Codes),
         append("solutions/", Codes, Sol_),
         append("grilles/", Codes, Name_),
         append(Name_, ".txt", Na),
         append(Sol_, ".txt", So),
-        string_to_list(Nam, Na),
-        string_to_list(Sol, So),
-        open(Nam, read, F),
-        read_lines(L, F),
-        close(F),
-        !,
-        transf2(L, G), !,
-        open(Sol, write, S),
-        % debug stuff: 
-        %trace,
-        %write(S, G),
-        sudoku_solve(G, [1, 2, 3, 4, 5, 6, 7, 8, 9], R),
-        predsort(sort_res, R, T),
-        prettyprint(S, T),
-        nl(S),
-        close(S), !,
+        string_to_list(I, Na),
+        string_to_list(O, So),
+        process(I, O),
         N2 is N-1,
-        test(N2, FR)
+        solve(N2)
 .
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
